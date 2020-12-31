98e7954e48ae16519021fbcbcb687f5b7beed5ae
kkulma <katarzyna.kulma@gmail.com>
2020-12-31 00:29:12 +0000

    added basic UI

---
+++ ui.R
@@ -0,0 +1,56 @@
+library(shinydashboard)
+library(leaflet)
+
+dashboardPage(
+  skin = "black",
+  dashboardHeader(title = "Flight Footprint"),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    fluidRow(
+      radioButtons(
+        inputId = "category",
+        label = "How to specify airports?",
+        choices = c(IATA = "iata", Coordinates = "coord"),
+        inline = TRUE
+      )
+    ),
+    conditionalPanel(
+      "input.category == 'iata'",
+      fluidRow(column(
+        6,
+        selectInput("outbound", "Departure Airport", c("AAA", "BBB", "CCC"))
+      ),
+      column(
+        6, selectInput("inbound", "Arrival Airport", c("AAA", "BBB", "CCC"))
+      )),
+      
+      fluidRow(column(
+        6, selectInput("class",
+                       "Flight Class",
+                       sort(
+                         c("Unknown", "Economy", "Economy+", "Business", "First")
+                       ))
+      ),
+      column(
+        6, selectInput("metric", "Footprint metric", c("co2e", "co2", "ch4", "n2o"))
+      ))
+    ),
+    conditionalPanel(
+      "input.category == 'coord'",
+      fluidRow(
+        column(6, numericInput("inlat", "Inbound Latittude", value = 0)),
+        column(6, numericInput("inlong", "Inbound Longitude", value = 0)),
+        fluidRow(column(
+          6, numericInput("outlat", "Outbound Latittude", value = 0)
+        ),
+        column(
+          6, numericInput("outlong", "Outbound Longitude", value = 0)
+        ))
+      )
+    ),
+    fluidRow(
+      leafletOutput("map")
+    )
+    
+  )
+)
