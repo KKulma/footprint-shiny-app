50f7e7d62467139cef51e97ae8965bf8aae054d6
kkulma <katarzyna.kulma@gmail.com>
2021-01-01 22:47:02 +0000

    moved libraries to global.R

--- server.R
+++ server.R
@@ -1,67 +1,70 @@
-library(shiny)
-library(shinydashboard)
-library(tidyverse)
-library(leaflet)
-library(footprint)
-library(airportr)
-library(geosphere)
-library(maps)
-library(dqshiny)
-
-server <- function(input, output) { 
-  
-
+server <- function(input, output) {
   # emissions calculation based on airport input ----
   
   emissions_text <- eventReactive(input$go, {
-    departure <- word(input$departure,1)
-    arrival <- word(input$arrival,1)
-    distance <- airportr::airport_distance(arrival, departure) %>% 
+    departure <- stringr::word(input$departure, 1)
+    arrival <- stringr::word(input$arrival, 1)
+    distance <- airportr::airport_distance(arrival, departure) %>%
       round(2)
-    estimate <- footprint::airport_footprint(arrival, departure, input$class, input$metric)
+    estimate <-
+      footprint::airport_footprint(arrival, departure, input$class, input$metric)
     
-    HTML(paste("<b>", departure, "</b> to <b>", arrival, "</b> (", distance, "km) Estimated Emissions: <br>",
-               "<span style='font-size: 160%; color: #3f9323;'><b>", estimate, "</b></span>", input$metric))
+    HTML(
+      paste(
+        "<b>",
+        departure,
+        "</b> to <b>",
+        arrival,
+        "</b> (",
+        distance,
+        "km) Estimated Emissions: <br>",
+        "<span style='font-size: 160%; color: #3f9323;'><b>",
+        estimate,
+        "</b></span>",
+        input$metric
+      )
+    )
   })
   
   output$emissions <- renderText({
     emissions_text()
   })
-
+  
   
   # map based on airport input -----
   
-  coord_data <- eventReactive(input$go,{
+  coord_data <- eventReactive(input$go, {
+    departure <- word(input$departure, 1)
+    arrival <- word(input$arrival, 1)
     
-    departure <- word(input$departure,1)
-    arrival <- word(input$arrival,1)
-  
-  lat_inbound <- airportr::airport_location(arrival) %>%
-    pull(Latitude)
-  long_inbound <- airportr::airport_location(arrival) %>%
-    pull(Longitude)
-  lat_outbound <- airportr::airport_location(departure) %>%
-    pull(Latitude)
-  long_outbound <- airportr::airport_location(departure) %>%
-    pull(Longitude)
-  
-  coord_data <- gcIntermediate(c(long_inbound, 
-                                            lat_inbound), 
-                                          c(long_outbound,
-                                            lat_outbound),
-                                          n=100, 
-                                          addStartEnd=TRUE,
-                                          sp=TRUE,
-                                          breakAtDateLine = T)
+    lat_inbound <- airportr::airport_location(arrival) %>%
+      pull(Latitude)
+    long_inbound <- airportr::airport_location(arrival) %>%
+      pull(Longitude)
+    lat_outbound <- airportr::airport_location(departure) %>%
+      pull(Latitude)
+    long_outbound <- airportr::airport_location(departure) %>%
+      pull(Longitude)
+    
+    coord_data <- gcIntermediate(
+      c(long_inbound,
+        lat_inbound),
+      c(long_outbound,
+        lat_outbound),
+      n = 100,
+      addStartEnd = TRUE,
+      sp = TRUE,
+      breakAtDateLine = T
+    )
   })
   
   
   
   output$map <- renderLeaflet({
-    coord_data() %>% 
-    leaflet() %>% 
-      addTiles(urlTemplate = "https://tile.jawg.io/jawg-dark/{z}/{x}/{y}.png?access-token=qk8Kpqcfoxfg04BI5egRq7tjX8COTiywPrjB2VYrxWQhtWRgnNhmWT3gCEDuobim") %>% 
+    coord_data() %>%
+      leaflet() %>%
+      addTiles(urlTemplate = "https://tile.jawg.io/jawg-dark/{z}/{x}/{y}.png?access-token=qk8Kpqcfoxfg04BI5egRq7tjX8COTiywPrjB2VYrxWQhtWRgnNhmWT3gCEDuobim") %>%
       addPolylines(color = "#6edf49")
   })
   
-  }
+}
--- ui.R
+++ ui.R
@@ -1,13 +1,3 @@
-library(shiny)
-library(shinydashboard)
-library(tidyverse)
-library(leaflet)
-library(footprint)
-library(airportr)
-library(geosphere)
-library(maps)
-library(dqshiny)
-
 # list of airports ----
 
 airports <- airportr::airports %>%
@@ -31,75 +21,85 @@ dashboardPage(
       radioButtons(
         inputId = "category",
         label = "Input Type:",
-        choices = c("IATA Airport Code" = "iata", 
-                    Coordinates = "coord", City = "city"),
+        choices = c(
+          "IATA Airport Code" = "iata",
+          Coordinates = "coord",
+          City = "city"
+        ),
         inline = TRUE
       )
     ),
     conditionalPanel(
       "input.category == 'iata'",
       fluidRow(
-        column(4,
-               autocomplete_input("departure", "Departure Airport:", airports, max_options = 5)),
-        column(4,
-               autocomplete_input("arrival", "Arrival Airport:", airports, max_options = 5))
+        column(
+          4,
+          dqshiny::autocomplete_input("departure", "Departure Airport:", airports, max_options = 5)
+        ),
+        column(
+          4,
+          dqshiny::autocomplete_input("arrival", "Arrival Airport:", airports, max_options = 5)
+        )
       ),
       fluidRow(column(
-        4, selectInput("class",
-                       "Flight Class",
-                       c("Economy", "Economy+", "Business", 
-                         "First", "Unknown")
-      )),
+        4, selectInput(
+          "class",
+          "Flight Class",
+          c("Economy", "Economy+", "Business",
+            "First", "Unknown")
+        )
+      ),
       column(
-        4, selectInput("metric", "Footprint metric*", 
-                       c("CO2e - Carbon Dioxide Equivalent" = "co2e",
-                         "CO2 - Carbon Dioxide" = "co2", 
-                         "CH4 - Methane" = "ch4",
-                         "N2O - Nitrous Oxide" = "n2o"))
+        4, selectInput(
+          "metric",
+          "Footprint metric*",
+          c(
+            "CO2e - Carbon Dioxide Equivalent" = "co2e",
+            "CO2 - Carbon Dioxide" = "co2",
+            "CH4 - Methane" = "ch4",
+            "N2O - Nitrous Oxide" = "n2o"
+          )
+        )
       ))
     ),
     conditionalPanel(
       "input.category == 'coord'",
-      fluidRow(
-        column(
-          4, numericInput("inlat", "Inbound Latittude", value = 0)
-        ),
-        column(
-          4, numericInput("inlong", "Outbound Longitude", value = 0)
-        )),
-        fluidRow(
-          column(
-          4, numericInput("outlat", "Outbound Latittude", value = 0)
-        ),
-        column(
-          4, numericInput("outlong", "Outbound Longitude", value = 0)
-        ))),
-    conditionalPanel(
-      "input.category == 'city'",
-      fluidRow(
-        column(4,
-               autocomplete_input("city1", "Departure City:", citydata, max_options = 5)),
-               column(4,
-                      autocomplete_input("city2", "Arrival City:", citydata, max_options = 5)
-               ))),
-    fluidRow(
+      fluidRow(column(
+        4, numericInput("inlat", "Inbound Latittude", value = 0)
+      ),
       column(
-        6, actionButton(inputId = "go", label = "Go!"),
-      )
-    ),
-    fluidRow(
-             HTML("<div align='center'>"),
-             htmlOutput("emissions"),
-             HTML("</div>")
-            ),
-    fluidRow(
-      leafletOutput("map")
-    ),
-    fluidRow(
+        4, numericInput("inlong", "Outbound Longitude", value = 0)
+      )),
+      fluidRow(column(
+        4, numericInput("outlat", "Outbound Latittude", value = 0)
+      ),
       column(
-        6,
-        p("* All estimates include radiative forcing")
-      )
-    
+        4, numericInput("outlong", "Outbound Longitude", value = 0)
+      ))
+    ),
+    conditionalPanel("input.category == 'city'",
+                     fluidRow(
+                       column(
+                         4,
+                         dqshiny::autocomplete_input("city1", "Departure City:", citydata, max_options = 5)
+                       ),
+                       column(
+                         4,
+                         dqshiny::autocomplete_input("city2", "Arrival City:", citydata, max_options = 5)
+                       )
+                     )),
+    fluidRow(column(
+      6, actionButton(inputId = "go", label = "Go!"),
+    )),
+    fluidRow(
+      shiny::HTML("<div align='center'>"),
+      htmlOutput("emissions"),
+      shiny::HTML("</div>")
+    ),
+    fluidRow(leaflet::leafletOutput("map")),
+    fluidRow(column(
+      6,
+      shiny::p("* All estimates include radiative forcing")
+    ))
   )
-))
+)
