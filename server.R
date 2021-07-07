f567baeef94ea772b020e3dfb75d2451659cd636
acircleda <acornrevolution@yahoo.com>
2021-07-06 20:00:58 -0400

    Updates to get all calculations working

--- server.R
+++ server.R
@@ -1,7 +1,11 @@
 server <- function(input, output) {
-  # emissions calculation based on airport input ----
+  
+  # map emissions text ----
   
   emissions_text <- eventReactive(input$go, {
+    
+    # emissions calculation based on airport input ----
+    if(input$category == "iata"){
     departure <- stringr::word(input$departure, 1)
     arrival <- stringr::word(input$arrival, 1)
     distance <- airportr::airport_distance(arrival, departure) %>%
@@ -9,21 +13,40 @@ server <- function(input, output) {
     estimate <-
       footprint::airport_footprint(arrival, departure, input$class, input$metric)
     
-    HTML(
-      paste(
-        "<b>",
-        departure,
-        "</b> to <b>",
-        arrival,
-        "</b> (",
-        distance,
-        "km) Estimated Emissions: <br>",
-        "<span style='font-size: 160%; color: #3f9323;'><b>",
-        estimate,
-        "</b></span>",
-        input$metric
-      )
-    )
+    # emissions calculation based on city input ----
+  } else if(input$category == "city") {
+      departure_lat <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city1) %>%
+        pull(lat)
+      departure_long <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city1) %>%
+        pull(long)
+      arrival_lat <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city2) %>%
+        pull(lat)
+      arrival_long <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city2) %>%
+        pull(long)
+      distance <- distance_calc(departure_long, departure_lat,
+                                arrival_long, arrival_lat) %>%
+        round(2)
+      estimate <-
+        footprint::latlong_footprint(departure_lat, departure_long,
+                                     arrival_lat, arrival_long,
+                                     input$class, input$metric)
+  }
+      HTML(
+        paste(
+          "Estimated Emissions: <br>",
+          "<span style='font-size: 160%; color: #3f9323;'><b>",
+          estimate,
+          "</b></span> kg",
+          input$metric
+        ))
   })
   
   output$emissions <- renderText({
@@ -34,6 +57,9 @@ server <- function(input, output) {
   # map based on airport input -----
   
   coord_data <- eventReactive(input$go, {
+    
+    if(input$category == "iata"){
+      
     departure <- word(input$departure, 1)
     arrival <- word(input$arrival, 1)
     
@@ -45,6 +71,26 @@ server <- function(input, output) {
       pull(Latitude)
     long_outbound <- airportr::airport_location(departure) %>%
       pull(Longitude)
+    
+    } else if(input$category == "city") {
+      lat_outbound <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city1) %>%
+        pull(lat)
+      long_outbound <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city1) %>%
+        pull(long)
+      lat_inbound <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city2) %>%
+        pull(lat)
+      long_inbound <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city2) %>%
+        pull(long)
+      
+    }
     
     coord_data <- gcIntermediate(
       c(long_inbound,
@@ -58,13 +104,163 @@ server <- function(input, output) {
     )
   })
   
-  
-  
+
   output$map <- renderLeaflet({
     coord_data() %>%
       leaflet() %>%
       addTiles(urlTemplate = "https://tile.jawg.io/jawg-dark/{z}/{x}/{y}.png?access-token=qk8Kpqcfoxfg04BI5egRq7tjX8COTiywPrjB2VYrxWQhtWRgnNhmWT3gCEDuobim") %>%
       addPolylines(color = "#6edf49")
+  })
+  
+  # personal carbon budget ----
+  
+  carbon_budget_text <- eventReactive(input$go, {
+    
+    # emissions calculation based on airport input ----
+    if(input$category == "iata"){
+      departure <- stringr::word(input$departure, 1)
+      arrival <- stringr::word(input$arrival, 1)
+      estimate <-
+        footprint::airport_footprint(arrival, departure, input$class, input$metric)
+      
+      # emissions calculation based on city input ----
+    } else if(input$category == "city") {
+      departure_lat <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city1) %>%
+        pull(lat)
+      departure_long <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city1) %>%
+        pull(long)
+      arrival_lat <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city2) %>%
+        pull(lat)
+      arrival_long <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city2) %>%
+        pull(long)
+      estimate <-
+        footprint::latlong_footprint(departure_lat, departure_long,
+                                     arrival_lat, arrival_long,
+                                    input$class, "co2e")
+    }
+    
+    pct <- scales::percent(round((estimate/1000)/4.8,3))
+    
+    HTML(
+      paste(
+        "This trip represents <br>",
+        "<span style='font-size: 160%; color: #3f9323;'><b>",
+        pct,
+        "</b></span><br> of your personal carbon budget for ",
+        format(Sys.Date(), "%Y")
+      ))
+  })
+  
+  output$carbon_budget <- renderText({
+    carbon_budget_text()
+  })
+  
+  # ice melt ----
+  
+  ice_melt_text <- eventReactive(input$go, {
+    
+    # emissions calculation based on airport input ----
+    if(input$category == "iata"){
+      departure <- stringr::word(input$departure, 1)
+      arrival <- stringr::word(input$arrival, 1)
+      estimate <-
+        footprint::airport_footprint(arrival, departure, input$class, input$metric)
+      
+      # emissions calculation based on city input ----
+    } else if(input$category == "city") {
+      departure_lat <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city1) %>%
+        pull(lat)
+      departure_long <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city1) %>%
+        pull(long)
+      arrival_lat <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city2) %>%
+        pull(lat)
+      arrival_long <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city2) %>%
+        pull(long)
+      estimate <-
+        footprint::latlong_footprint(departure_lat, departure_long,
+                                     arrival_lat, arrival_long,
+                                     input$class, "co2")
+    }
+    
+    ice <- round((estimate/1000)*3,2)
+    
+    HTML(
+      paste(
+        "This trip melted about <br>",
+        "<span style='font-size: 160%; color: #3f9323;'><b>",
+        ice,
+        "</b></span><br> square meters of arctic ice <br/>"
+      ))
+  })
+  
+  output$ice_melt <- renderText({
+    ice_melt_text()
+  })
+  
+  # tree planting ----
+  
+  tree_text <- eventReactive(input$go, {
+    
+    # emissions calculation based on airport input ----
+    if(input$category == "iata"){
+      departure <- stringr::word(input$departure, 1)
+      arrival <- stringr::word(input$arrival, 1)
+      estimate <-
+        footprint::airport_footprint(arrival, departure, input$class, input$metric)
+      
+      # emissions calculation based on city input ----
+    } else if(input$category == "city") {
+      departure_lat <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city1) %>%
+        pull(lat)
+      departure_long <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city1) %>%
+        pull(long)
+      arrival_lat <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city2) %>%
+        pull(lat)
+      arrival_long <- maps::world.cities %>%
+        mutate(name2 = paste(name, ", ", country.etc)) %>%
+        filter(name2 == input$city2) %>%
+        pull(long)
+      estimate <-
+        footprint::latlong_footprint(departure_lat, departure_long,
+                                     arrival_lat, arrival_long,
+                                     input$class, "co2")
+    }
+    
+    trees <- round((estimate/1000)/.06)
+    
+    HTML(
+      paste(
+        "It would take <br>",
+        "<span style='font-size: 160%; color: #3f9323;'><b>",
+        trees,
+        "</b></span><br> new trees planted to offset this trip<br/>"
+      ))
+  })
+  
+  output$trees <- renderText({
+    tree_text()
   })
   
 }
--- ui.R
+++ ui.R
@@ -11,11 +11,27 @@ citydata <- maps::world.cities %>%
   mutate(name2 = paste(name, ", ", country.etc)) %>%
   select(name2) %>% pull()
 
+# distance function ----
+distance_calc <- function(departure_long, departure_lat, 
+                     arrival_long, arrival_lat){
+
+    lon1 = departure_long * pi/180
+    lat1 = departure_lat * pi/180
+    lon2 = arrival_long * pi/180
+    lat2 = arrival_lat * pi/180
+    radius = 6373
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = (sin(dlat/2))^2 + cos(lat1) * cos(lat2) * (sin(dlon/2))^2
+    b = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = radius * b
+}
+
 # begin dashboard ----
 
 dashboardPage(
   skin = "black",
-  dashboardHeader(title = "Flight Footprint Calculator"),
+  dashboardHeader(title = "Air Travel Footprint Calculator"),
   dashboardSidebar(disable = TRUE),
   dashboardBody(
     tags$head(
@@ -27,8 +43,8 @@ dashboardPage(
         label = "Input Type:",
         choices = c(
           "IATA Airport Code" = "iata",
-          Coordinates = "coord",
           City = "city"
+          #Coordinates = "coord"
         ),
         inline = TRUE
       )
@@ -66,21 +82,6 @@ dashboardPage(
         )
       ))
     ),
-    conditionalPanel(
-      "input.category == 'coord'",
-      fluidRow(column(
-        4, numericInput("inlat", "Inbound Latittude", value = 0)
-      ),
-      column(
-        4, numericInput("inlong", "Outbound Longitude", value = 0)
-      )),
-      fluidRow(column(
-        4, numericInput("outlat", "Outbound Latittude", value = 0)
-      ),
-      column(
-        4, numericInput("outlong", "Outbound Longitude", value = 0)
-      ))
-    ),
     conditionalPanel("input.category == 'city'",
                      fluidRow(
                        column(
@@ -91,7 +92,42 @@ dashboardPage(
                          4,
                          dqshiny::autocomplete_input("city2", "Arrival City:", citydata, max_options = 5)
                        )
-                     )),
+                     ),
+                     fluidRow(column(
+                       4, selectInput(
+                         "class",
+                         "Flight Class",
+                         c("Economy", "Economy+", "Business",
+                           "First", "Unknown")
+                       )
+                     ),
+                     column(
+                       4, selectInput(
+                         "metric",
+                         "Footprint metric*",
+                         c(
+                           "CO2e - Carbon Dioxide Equivalent" = "co2e",
+                           "CO2 - Carbon Dioxide" = "co2",
+                           "CH4 - Methane" = "ch4",
+                           "N2O - Nitrous Oxide" = "n2o"
+                         )
+                       )
+                     ))),
+    # conditionalPanel(
+    #   "input.category == 'coord'",
+    #   fluidRow(column(
+    #     4, numericInput("inlat", "Inbound Latittude", value = 0)
+    #   ),
+    #   column(
+    #     4, numericInput("inlong", "Outbound Longitude", value = 0)
+    #   )),
+    #   fluidRow(column(
+    #     4, numericInput("outlat", "Outbound Latittude", value = 0)
+    #   ),
+    #   column(
+    #     4, numericInput("outlong", "Outbound Longitude", value = 0)
+    #   ))
+    # ),
     fluidRow(column(
       6, actionButton(inputId = "go", label = "Go!"),
     )),
@@ -107,10 +143,32 @@ dashboardPage(
           shiny::HTML("</div>"), #stack-top div
       shiny::HTML("</div>")), #container div
       column(6,
-             shiny::p("Equivalents"))
+             p(htmlOutput("carbon_budget")),
+             p(htmlOutput("ice_melt")),
+             p(htmlOutput("trees")),
+             )
     ),
     fluidRow(
       shiny::p("* All estimates include radiative forcing")
+    ),
+    box(
+      title = "Methodology", solidHeader = TRUE,
+      collapsible = TRUE,
+      collapsed = TRUE,
+      width = 10,
+      h3("Emissions"),
+      HTML(
+        paste(
+          "Emissions are calculated with the <a href='https://github.com/acircleda/footprint' target='_blank'>footprint R package</a>.")),
+      h3("Personal Carbon Footprint"),
+      HTML("The personal carbon footprint for 2020-2030 ranges from 3.7 to 4.8 metric tons of CO2e per person. This range is dervived from a number of scenarios that limit global warming to 1.5 or 2 degrees Celsius above pre-industrial temperatures. For our calculator's estimate, we divide the metric tons of CO2e from the inputted trip by the upper range of the carbon budget, 4.8.",
+           "<br><br>Source: <a href='https://www.iges.or.jp/en/pub/15-degrees-lifestyles-2019/en' target='_blank'>1.5-Degree Lifestyles: Targets and options for reducing lifestyle carbon footprints</a>"),
+      h3("Arctic Ice Melt"),
+      HTML("This estimate is based on a linear relationship between CO2e and arctice ice melt at a rate of approximately 3 square meters per metric ton of CO2",
+           "<br><br>Source: <a href='https://doi.org/10.1126/science.aag2345' target='_blank'>Observed Arctic sea-ice loss directly follows anthropogenic CO2 emission</a>"),
+      h3("Tree Planting"),
+      HTML("The number of newly-planted trees required to offset a trip specifically refers to the number of urban medium growth coniferous or deciduous tree seedlings grown for 10 years in the United States. It is estimated that one tree in this scenario can sequester .060 metric tons of CO2.",
+           "<br><br>Source: <a href='file:///C:/Users/aschmidt/Zotero/storage/2YEFXCII/greenhouse-gases-equivalencies-calculator-calculations-and-references.html' target='_blank'>Greenhouse Gases Equivalencies Calculator - Calculations and References</a>"),
     )
   )
 )
